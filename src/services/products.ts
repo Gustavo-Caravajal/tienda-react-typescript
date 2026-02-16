84967ab6b9a34c2e257dd3de473f23b60000e68e
Gustavo Caravajal <Gustavocarvajal_2001@outlook.es>
2026-02-15 21:08:02 -0300

    feat: agregar funcion para crear los productos, y agregar el servicio 'uploadImage.ts'

--- src/services/products.ts
+++ src/services/products.ts
@@ -1,13 +1,30 @@
 import type { Product } from "../types/Product";
 
+const BASE_URL: string = ""
 
 export const getProducts = async (): Promise<Product[]> => {
     const res = await fetch("/data/products.json");
 
-    if(!res.ok){
+    if (!res.ok) {
         throw new Error("La peticion a los productos fallo");
     }
-    
+
     const results: Product[] = await res.json();
     return results;
+}
+
+export const createProduct = async (product: Product): Promise<Product> => {
+    const res = await fetch(BASE_URL, {
+        method: "POST",
+        headers: { "Content-type": "application/json" },
+        body: JSON.stringify(product)
+    });
+
+    if(!res.ok){
+        throw new Error("No se pudo crear el producto");
+    }
+
+    const result = res.json();
+    return result;
+
 }
